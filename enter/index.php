93de019561b746fdc125028718c4de76628bc2cd
Zwitschi <zwitschi82@gmail.com>
2012-03-27 22:24:50 +0200

    first commit

---
+++ enter/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Piratenpartei Österreich</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<header class="section header">
+	<div id="logo"><h1>LOGO</h1></div>
+</header>
+<div id="main-content" class="section content">
+	<div class="row">
+		<a id="werden" class="boxen_1">
+			<div class="formheader">
+				<h2>Mach Mit!</h2>
+			</div>
+			<p>Werde Pirat!</p>
+		</a>
+		<a id="login" class="boxen_1">
+			<div class="formheader">
+				<h2>Unterstütze uns</h2>
+			</div>
+			<p>Mit einer Spende und bereits mit deinem Mitgliedsbeitrag</p>
+		</a>
+		<div class="clear"></div>
+	</div>
+</div>
+<footer id="footer" class="section footer">
+    <div class="row">
+        <div class="first-footer-widget-area">
+	<div class="skin">
+				<div class="widget"><h3 class="widget-title">Piratenparteien weltweit</h3>			<div class="textwidget"><ul class="xoxo blogroll">
+<li><a href="http://www.partidopirata.com.ar/">Argentinien</a></li>
+<li><a href="http://pirateparty.org.au/">Australien</a></li>
+<li><a href="http://pirateparty.be/">Belgien</a></li>
+<li><a href="http://www.partidopirata.org/">Brasilien</a></li>
+<li><a href="http://piratskapartia.bg/">Bulgarien</a></li>
+<li><a href="http://www.partidopirata.cl/">Chile</a></li>
+<li><a href="http://piratpartiet.dk/">Dänemark</a></li>
+<li><a href="http://www.piraattipuolue.fi/">Finnland</a></li>
+<li><a href="http://partipirate.org/">Frankreich</a></li>
+<li><a href="http://pirateparty.gr/">Griechenland</a></li>
+<li><a href="http://partidopirata.org.gt/">Guatemala</a></li>
+<li><a href="http://www.pp-international.net/">International</a></li>
+<li><a href="http://www.partito-pirata.it/">Italien</a></li>
+<li><a href="http://www.piratepartyofcanada.com/">Kanada</a></li>
+<li><a href="http://pirateparty.kz/">Kasachstan</a></li>
+<li><a href="http://pp.interlecto.net/">Kolumbien</a></li>
+<li><a href="http://piratupartija.lv/">Lettland</a></li>
+<li><a href="http://piratupartija.lt/">Litauen</a></li>
+<li><a href="http://www.piratepartei.lu/">Luxemburg</a></li>
+<li><a href="http://partipirate.ma/">Marokko</a></li>
+<li><a href="http://www.partidopiratamexicano.org/">Mexiko</a></li>
+<li><a href="http://pirateparty.org.nz/">Neuseeland</a></li>
+<li><a href="http://www.piratenpartij.nl/">Niederlande</a></li>
+<li><a href="http://piratenpartei.at/">Österreich</a></li>
+<li><a href="http://wiki.freeculture.org/Pirata">Peru</a></li>
+<li><a href="http://www.partiapiratow.org.pl/">Polen</a></li>
+<li><a href="http://www.partidopirata.co.cc/">Portugal</a></li>
+<li><a href="http://www.partidulpiratilor.ro/">Rumänien</a></li>
+<li><a href="http://pirate-party.ru/">Russland</a></li>
+<li><a href="http://www.piratpartiet.se/">Schweden</a></li>
+<li><a href="http://www.piratenpartei.ch/">Schweiz</a></li>
+<li><a href="http://www.piratskapartija.com/">Serbien</a></li>
+<li><a href="http://www.piratskastrana.sk/">Slowakei</a></li>
+<li><a href="http://www.piratskastranka.net/">Slowenien</a></li>
+<li><a href="http://www.partidopirata.es/">Spanien</a></li>
+<li><a href="http://pirateparty.kr/">Südkorea</a></li>
+<li><a href="http://www.ceskapiratskastrana.cz/">Tschechien</a></li>
+<li><a href="http://partipirate-tn.org/">Tunesien</a></li>
+<li><a href="http://www.korsanpartisi.org/">Türkei</a></li>
+<li><a href="http://pp-ua.org/">Ukraine</a></li>
+<li><a href="http://partidopirata.org.uy/">Uruguay</a></li>
+<li><a href="http://pirate-party.us/">USA</a></li>
+<li><a href="http://pirateparty.org.uk/">Vereinigtes Königreich</a></li>
+<li><a href="http://belpirat.blog.tut.by/">Weißrussland</a></li>
+<li><a href="http://www.piratepartycyprus.com/">Zypern</a></li>
+
+	</ul></div>
+		</div>			</div>
+</div>
+        <div class="second-footer-widget-area">
+	<div class="skin">
+				<div class="widget"><h3 class="widget-title">Piratenpartei Österreich</h3>			<div class="textwidget"></div>
+		</div>				<div class="menu-header"><ul id="menu-submenu" class="menu"><li id="menu-item-1599" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1599"><a href="http://piratenpartei.at/">Kontakt</a></li>
+<li id="menu-item-1783" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1783"><a href="http://piratenpartei.at">CC-BY-SA 3.0</a></li>
+</ul></div>	</div>
+</div>
+</div>
+
+</footer>
+</body>
+</html>
